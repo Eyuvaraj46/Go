5118c94deb4c9c971815ecb18c6cd6611bdf8a8d
Eyuvaraj46 <yuvarajelangovan333@gmail.com>
2025-04-29 14:13:28 +0530

    map

---
+++ map.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main(){
+	fmt.Println("learn abt map in golang")
+	languages := make(map[string]string)
+	languages["JS"]="javascript"
+	languages["RB"]="ruby"
+	languages["PY"]="python"
+	fmt.Println("list of all languages:",languages)
+	fmt.Println("js shorts for:",languages["JS"])
+
+
+
+
+
+	}	
+
